5e4d1ad2242b38cc21b1e20fa49ffb87ebbb5464
usamafayaz <usama.fayyaz157@gmail.com>
2024-11-26 21:27:46 +0500

    Removed unnecessary fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,16 +8,6 @@ const poppins = Poppins({
   subsets: ["latin"],
   display: "swap",
 });
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -27,9 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased ${poppins.className}`}
-      >
+      <body className={`${poppins.className}`}>
         <div className="flex flex-col min-h-screen">
           <Header />
           <main className="flex-grow">{children}</main>
